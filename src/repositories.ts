10e146ec693731122bf89506b59192b1ab345714
Vinícios <vviniciosc@gmail.com>
2023-11-20 16:27:13 -0300

    fix: changed names for better understanding

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,9 +1,9 @@
 import { AppDataSource } from "./data-source";
 import { Contact } from "./entities/Contact.entity";
 import { User } from "./entities/User.entity";
-import { TUserRepo } from "./interfaces/user.interfaces";
+import { TUserRepo } from "./interfaces/users.interfaces";
 
-const userRepo: TUserRepo = AppDataSource.getRepository(User);
-const contactRepo = AppDataSource.getRepository(Contact);
+const userRepository: TUserRepo = AppDataSource.getRepository(User);
+const contactRepository = AppDataSource.getRepository(Contact);
 
-export { userRepo, contactRepo };
+export { userRepository, contactRepository };
