73bec306ad8c798348a35f0c0fa607e0e65fdf50
Vinícios <vviniciosc@gmail.com>
2023-11-20 15:43:00 -0300

    feat: created contact schema

---
+++ src/schemas/contact.schemas.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { userSchema } from "./user.schemas";
+
+const contactSchema = z.object({
+  id: z.string(),
+  fullName: z.string().max(255),
+  email: z.string().email().max(45),
+  telephone: z.number(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  deletedAt: z.string().nullish().optional(),
+  user: userSchema,
+});
+
+const contactRequestSchema = contactSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  deletedAt: true,
+  user: true,
+});
+
+const contactUpdateSchema = contactSchema
+  .omit({
+    id: true,
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    user: true,
+  })
+  .partial();
+
+const contactsResponseSchema = contactSchema.array();
+
+export {
+  contactSchema,
+  contactRequestSchema,
+  contactUpdateSchema,
+  contactsResponseSchema,
+};
