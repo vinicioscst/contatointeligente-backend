202107ad8579aaa485631b929a326d0684a05187
Vinícios <vviniciosc@gmail.com>
2023-11-20 22:16:06 -0300

    fix: fixed middlewares order in users route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,7 +9,7 @@ import { validateUserPermission } from "../middlewares/validateUserPermission.mi
 
 const usersRouter: Router = Router()
 
-usersRouter.use("/:id", validateUserId, validateToken, validateUserPermission)
+usersRouter.use("/:id", validateToken, validateUserPermission, validateUserId)
 
 usersRouter.post("", validateBody(userRequestSchema), validateEmail, (req, res) => usersController.create(req, res))
 usersRouter.get("", (req, res) => usersController.listAll(req, res))
