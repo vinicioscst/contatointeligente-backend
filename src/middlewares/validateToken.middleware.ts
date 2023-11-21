91c57a00dec153b76c2c4934fc2785f712e14e0a
Vinícios <vviniciosc@gmail.com>
2023-11-21 17:12:39 -0300

    fix: error messages improved

--- src/middlewares/validateToken.middleware.ts
+++ src/middlewares/validateToken.middleware.ts
@@ -9,11 +9,11 @@ export const validateToken = (
 ): void => {
   const authorization: string | undefined = req.headers.authorization;
 
-  if (!authorization) {
-    throw new AppError("Missing token", 401);
-  }
+  if (!authorization) throw new AppError("Missing token", 401)
 
   const token = authorization.split(" ")[1];
+
+  if (!token) throw new AppError("Missing token", 401);
 
   verify(token, process.env.SECRET_KEY!, (error: any, decoded: any) => {
     if (error) {
