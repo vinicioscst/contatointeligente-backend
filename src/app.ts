46d46ce153ce1fc483956a8095e7bf62030aa546
Vinícios <vviniciosc@gmail.com>
2023-11-20 21:43:43 -0300

    feat: added users route and errors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata"
 import "express-async-errors"
 import express from "express"
+import { usersRouter } from "./routes/users.routes"
+import { handleErrors } from "./middlewares/handleErrors"
 
 const app = express()
 
 app.use(express.json())
+app.use("/users", usersRouter)
 
+
+app.use(handleErrors)
 
 export default app
