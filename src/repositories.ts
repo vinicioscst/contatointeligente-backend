8dd211122a3d58fba3a7765b23e04984ab0598a7
Vinícios <vviniciosc@gmail.com>
2023-11-20 21:32:57 -0300

    feat: updated repositories file

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,9 +1,10 @@
 import { AppDataSource } from "./data-source";
 import { Contact } from "./entities/Contact.entity";
 import { User } from "./entities/User.entity";
+import { TContactRepo } from "./interfaces/contacts.interfaces";
 import { TUserRepo } from "./interfaces/users.interfaces";
 
 const userRepository: TUserRepo = AppDataSource.getRepository(User);
-const contactRepository = AppDataSource.getRepository(Contact);
+const contactRepository: TContactRepo = AppDataSource.getRepository(Contact);
 
 export { userRepository, contactRepository };
