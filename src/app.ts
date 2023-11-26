696ac8a741bfe6b0b22a53374790cc4b18d664c1
Vinícios <vviniciosc@gmail.com>
2023-11-26 04:38:06 -0300

    fix: install and configured cors to allow request from localhost

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,15 @@ import { contactsRouter } from "./routes/contacts.routes"
 
 const app = express()
 
+const cors = require('cors')
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
+
+
 app.use(express.json())
 app.use("/users", usersRouter)
 app.use("/login", sessionRouter)
